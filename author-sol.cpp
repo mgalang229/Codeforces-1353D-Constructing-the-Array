1d0fff8c67240c312227127a2aecaf65da23f928
jasonpogi <51401355+mgalang229@users.noreply.github.com>
2023-07-07 00:09:15 +0800

    Update author-sol.cpp

--- author-sol.cpp
+++ author-sol.cpp
@@ -2,34 +2,47 @@
 using namespace std;
 
 struct cmp {
-	bool operator() (const pair<int, int> &a, const pair<int, int> &b) const {
-		int lena = a.second - a.first + 1;
-		int lenb = b.second - b.first + 1;
-		if (lena == lenb) return a.first < b.first;
-		return lena > lenb;
+	bool operator() (const pair<int, int>& a, const pair<int, int>& b) const {
+		int len_a = a.second - a.first + 1;
+		int len_b = b.second - b.first + 1;
+		if(len_a == len_b) {
+			return a.first < b.first;
+		}
+		return len_a > len_b;
 	}
 };
 
-int main() {
-	int t;
-	cin >> t;
-	while (t--) {
-		int n;
-		cin >> n;
-		set<pair<int, int>, cmp> segs;
-		segs.insert({0, n - 1});
-		vector<int> a(n);
-		for (int i = 1; i <= n; ++i) {
-			pair<int, int> cur = *segs.begin();
-			segs.erase(segs.begin());
-			int id = (cur.first + cur.second) / 2;
-			a[id] = i;
-			if (cur.first < id) segs.insert({cur.first, id - 1});
-			if (id < cur.second) segs.insert({id + 1, cur.second});
+void test_case(int& tc) {
+	int n;
+	cin >> n;
+	set<pair<int, int>, cmp> segs;
+	segs.insert(make_pair(0, n - 1));
+	int ans[n];
+	for(int i = 1; i <= n; i++) {
+		pair<int, int> cur = *segs.begin();
+		segs.erase(segs.begin());
+		int mid = (cur.first + cur.second) / 2;
+		ans[mid] = i;
+		if(cur.first < mid) {
+			segs.insert(make_pair(cur.first, mid - 1));
 		}
-		for (auto it : a) cout << it << " ";
-		cout << endl;
+		if(mid < cur.second) {
+			segs.insert(make_pair(mid + 1, cur.second));
+		}
 	}
+	for(int& x : ans) {
+		cout << x << " ";
+	}
+	cout << "\n";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
 	
-	return 0;
+	int T = 1;
+	cin >> T;
+	for(int tc = 1; tc <= T; tc++) {
+		test_case(tc);
+	}
 }
